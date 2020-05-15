b0d871204ecbb435e83836a49d454db04158598e
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-15 11:20:16 +0300

    Update KafkaUninstall.bash

--- KafkaUninstall.bash
+++ KafkaUninstall.bash
@@ -4,7 +4,6 @@ rm -rf /etc/systemd/system/kafka.service
  
 userdel -r kafka
  
-firewall-cmd --permanent --remove-rich-rule 'rule family="ipv4" source address="10.233.234.0/24" port port="9093" protocol="tcp" accept'
-firewall-cmd --permanent --remove-rich-rule 'rule family="ipv4" source address="10.233.0.0/24" port port="9093" protocol="tcp" accept'
+firewall-cmd --permanent --remove-rich-rule 'rule family="ipv4" source address="10.0.0.0/24" port port="9093" protocol="tcp" accept'
 firewall-cmd  --reload
 firewall-cmd  --list-all
