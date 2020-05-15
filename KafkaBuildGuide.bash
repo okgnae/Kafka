73fc0736317ca587fa1060066f9e7f1483e89749
Reangko <33633448+reangko@users.noreply.github.com>
2020-05-15 09:22:27 +0300

    Update KafkaBuildGuide.bash

--- KafkaBuildGuide.bash
+++ KafkaBuildGuide.bash
@@ -1,38 +1,29 @@
-###
 ### Set BROKER_ID to {1,2,3} depending on which Broker your working on
 ### Passwords are set for refrance in lab, dont write passwords in scripts in production 
 BROKER_ID=1
 KAFKA_PASS=kafka123
 
-###
 ### Download Kafka tarball from apache
 curl --url https://downloads.apache.org/kafka/2.5.0/kafka_2.12-2.5.0.tgz --output /tmp/kafka_2.12-2.5.0.tgz
 
-###
 ### Move kafka tgz file from tmp to opt
 mv /tmp/kafka_2.12-2.5.0.tgz /opt
 
-###
 ### Untar file
 tar -zxf /opt/kafka_2.12-2.5.0.tgz -C /opt
 
-###
 ### Rename Kafka directory name
 mv  /opt/kafka_2.12-2.5.0  /opt/kafka
 
-###
 ### Create Data directory
 mkdir /opt/kafka/data
 
-###
 ### Make SSL Directory
 mkdir /opt/kafka/ssl
 
-###
 ### Add user. Will cause warning
 useradd -u 996 -U kafka -m -d /opt/kafka -s /sbin/nologin
 
-###
 ### Create Kafka service unit
 cat << EOF > /etc/systemd/system/kafka.service
 [Unit]
@@ -52,15 +43,12 @@ ExecStart=/opt/kafka/bin/kafka-server-start.sh  /opt/kafka/config/server.propert
 WantedBy=multi-user.target
 EOF
 
-###
 ### Reload Systemd Service units
 systemctl daemon-reload
 
-###
 ### Increase max java heap to 3GB (change as needed) in /opt/kafka/bin/kafka-server-start.sh
 sed -i 's/ export KAFKA_HEAP_OPTS=.*/ export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"/' /opt/kafka/bin/kafka-server-start.sh
 
-###
 ### Server.properties file Configurations
 sed -i "s/^broker.id=.*/broker.id=${BROKER_ID}/"  /opt/kafka/config/server.properties
 sed -i 's/^.*delete.topic.enable=.*/delete.topic.enable=true/'  /opt/kafka/config/server.properties
@@ -73,7 +61,6 @@ sed -i 's/^num.partitions=.*/num.partitions=6/'  /opt/kafka/config/server.proper
 sed -i 's/^log.retention.hours=.*/log.retention.hours=12/'  /opt/kafka/config/server.properties
 sed -i 's/^zookeeper.connect=.*/zookeeper.connect=KAFKA0001.hq.corp:2181,KAFKA0002.hq.corp:2181,KAFKA0003.hq.corp:2181/'  /opt/kafka/config/server.properties
 
-###
 ### SSL Configs for /opt/kafka/config/server.properties, you will have to fill in PASSWORD
 cat << EOF >> /opt/kafka/config/server.properties
 
@@ -91,7 +78,6 @@ ssl.truststore.password=${KAFKA_PASS}
 ssl.client.auth=none
 EOF
 
-###
 ### More Configs for /opt/kafka/config/server.properties
 cat << EOF >> /opt/kafka/config/server.properties
 
@@ -108,11 +94,9 @@ fetch.max.bytes=15728640
 replica.fetch.wait.max.ms=5000
 EOF
 
-###
 ### Make /etc/kafka/ssl 
 mkdir /etc/kafka
 mkdir /etc/kafka/ssl
-
 
 #######################################################################################
 ###### Use this following setion if you dont alreay have a Certificate Authority ######
@@ -120,9 +104,7 @@ mkdir /etc/kafka/ssl
 ### This is only done on broker 1. All other certs need to be signed by broker 1 CA ###
 #######################################################################################
 
-###
 ### CA Passphrase for the this lab will be "ca123"
-
 ### Make a CA directory
 mkdir /etc/kafka/ca
 
@@ -151,14 +133,13 @@ openssl x509 -noout -text -in /etc/kafka/ca/kafka-interm-ca-1.cer
 echo 00 > /etc/kafka/ca/kafka-interm-ca-1.srl
 
 ### Sign CSRs with the intermediate CA, you will have to make you CSRs on each broker
-# openssl x509 -req -days 3650 -CA /etc/kafka/ca/kafka-interm-ca-1.cer -CAkey /etc/kafka/ca/kafka-interm-ca-1.key -CAserial /etc/kafka/ca/kafka-interm-ca-1.srl -in /etc/kafka/ca/${BROKER_CSR} -out /etc/kafka/ca/${BROKER_CER}
+### openssl x509 -req -days 3650 -CA /etc/kafka/ca/kafka-interm-ca-1.cer -CAkey /etc/kafka/ca/kafka-interm-ca-1.key -CAserial /etc/kafka/ca/kafka-interm-ca-1.srl -in /etc/kafka/ca/${BROKER_CSR} -out /etc/kafka/ca/${BROKER_CER}
 
 ################################################################
 ### If you created a CA on Broker1 one you will need to copy ###
 ####### all other brokers CSRs there to have them signed #######
 ################################################################
 
-###
 ### Create Kafka Brokers Private Key and CSR
 openssl req -nodes -newkey rsa:2048 -keyout /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.key  -out /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.csr -subj "/C=US/O=corp/OU=hq/CN=KAFKA000${BROKER_ID}.hq.corp"
 
@@ -167,68 +148,55 @@ openssl req -nodes -newkey rsa:2048 -keyout /etc/kafka/ssl/KAFKA000${BROKER_ID}.
 ### If not then sign the Broker CSR with the kafka-interm-ca-1 ###
 ##################################################################
 
-###
 ### sign the Broker CSR with kafka-interm-ca-1
 openssl x509 -req -days 3650 -CA /etc/kafka/ca/kafka-interm-ca-1.crt -CAkey /etc/kafka/ca/kafka-interm-ca-1.key -CAserial /etc/kafka/ca/kafka-interm-ca-1.srl -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.csr -out /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.crt
 
-###
 ### Check keys, CSRs and certs
 openssl req -text -noout -verify -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.csr
 openssl rsa -check -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.key
 openssl x509 -noout -text -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.cer
 
-###
 ### Combine Server .key and the Server .cer files to create the server .p12. Use same passwords as set in SSL CONFIGS section of the sever.properties file
 openssl pkcs12 -export -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.crt -inkey /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.key -name KAFKA000${BROKER_ID}.hq.corp.p12 -out /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.p12
 openssl pkcs12 -info -in /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.p12
 
-###
 ### Create Server Key Store and add certs. Use same passwords as set in SSL CONFIGS section of the sever.properties file
 keytool -importkeystore -srckeystore /etc/kafka/ssl/KAFKA000${BROKER_ID}.hq.corp.p12 -destkeystore /opt/kafka/ssl/server.keystore.jks -srcstoretype pkcs12 -alias KAFKA000${BROKER_ID}.hq.corp.p12
 keytool -keystore /opt/kafka/ssl/server.keystore.jks -alias kafka-root-ca-1 -import -file "/etc/kafka/ca/kafka-root-ca-1.crt"
 keytool -keystore /opt/kafka/ssl/server.keystore.jks -alias kafka-interm-ca-1 -import -file "/etc/kafka/ca/kafka-interm-ca-1.crt"
 
-###
 ### Create Server Trust Store and add CA Root Cert. Use same passwords as set in SSL CONFIGS section of the sever.properties file
 keytool -keystore /opt/kafka/ssl/server.truststore.jks -alias kafka-root-ca 1 -import -file "/etc/kafka/ca/kafka-root-ca-1.crt"
 keytool -keystore /opt/kafka/ssl/server.truststore.jks -alias kafka-interm-ca-1 -import -file "/etc/kafka/ca/kafka-interm-ca-1.crt"
 
-###
 ### Create Client Trust Store and add CA certs. Use same passwords as set in SSL CONFIGS section of the sever.properties file
 keytool -keystore /opt/kafka/ssl/client.truststore.jks -alias kafka-root-ca-1 -import -file "/etc/kafka/ca/kafka-root-ca-1.crt"
 keytool -keystore /opt/kafka/ssl/client.truststore.jks -alias kafka-interm-ca-1 -import -file "/etc/kafka/ca/kafka-interm-ca-1.crt"
 
-###
 ### List certs in stores to validate
 keytool -list -keystore /opt/kafka/ssl/server.keystore.jks
 keytool -list -keystore /opt/kafka/ssl/server.truststore.jks
 keytool -list -keystore /opt/kafka/ssl/client.truststore.jks
 
-###
 ### Backup Key Stores to /etc/kafka/ssl
 cp  /opt/kafka/ssl/*.jks  /etc/kafka/ssl
 
-###
 ### Correct Kafka directory permissions
 chown -R kafka:kafka /opt/kafka
 chmod -R 700 /opt/kafka
 chown -R root:root  /etc/kafka
 chmod -R 700 /etc/kafka
 
-###
 ### Configuring Firewall
 firewall-cmd --permanent --add-rich-rule 'rule family="ipv4" source address="10.0.0.0/24" port port="9093" protocol="tcp" accept'
 firewall-cmd --reload
 firewall-cmd --list-all
 
-###
 ### Set Kafka Service to start on boot
 systemctl enable kafka
 
-###
 ### Start Kafka Service
 systemctl start kafka
 
-###
 ### Status Kafka Service
 systemctl  status  kafka
